465054683479c448162fe52b86be316172b9d457
feslipebraga <feslipebraga@gmail.com>
2023-08-16 11:14:41 -0300

    new

--- Udemy/Exercices/014. TV Series Challenges.sql
+++ Udemy/Exercices/014. TV Series Challenges.sql
@@ -11,3 +11,13 @@ join reviews on reviews.series_id = series.id
 group by title
 order by avg_rating;
 
+-- #3
+
+select first_name, last_name, rating from reviewers
+join reviews on reviews.reviewer_id = reviewers.id;
+
+-- #4
+
+select title as unreviewed_series from series
+left join reviews on reviews.series_id = series.id
+where rating IS NULL;
